ca057968bc96bd5d824f4753bbf21b8cbb5bbcef
RobotGrrl <erin@robotgrrl.com>
2015-10-27 08:58:44 -0400

    jotting down notes

--- expr_eval.py
+++ expr_eval.py
@@ -8,5 +8,16 @@ res1 = eval(expr1)
 print "1: %s" % (res1)
 
 expr2 = "c1.x + c0.len"
+new1 = expr2.replace("c1.x", "a")
+new2 = new1.replace("c0.len", "b")
+print "2: %s" % (new2)
 
+expr3 = "c1.x + c0.len"
+new1 = expr3.replace("c1.x", str(c1_x))
+new2 = new1.replace("c0.len", str(c0_len))
+print "3: %s" % (new2)
+
+expr4 = new2
+res1 = eval(expr4)
+print "4: %s" % (res1)
 
