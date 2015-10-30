e6c7029f89378f9f2db215c602e35a31bf8fcd99
RobotGrrl <erin@robotgrrl.com>
2015-10-30 17:43:13 -0400

    figured it out

--- compute_params.py
+++ compute_params.py
@@ -7,12 +7,15 @@ all_nodes = []
 
 class Node:
   """Each node in the Antimony file"""
-  uid = 0
-  name = ""
-  parameters = []
+  #uid = 0
+  #name = ""
+  #props = []
 
-  #def __init__(self):
-    
+  def __init__(self):
+    self.uid = 0
+    self.name = ""
+    self.props = []
+
 
 with open(antimony_file, "r+") as json_file:
 
@@ -21,6 +24,8 @@ with open(antimony_file, "r+") as json_file:
   nodes = json_data['nodes']
 
   num_nodes = len(nodes)
+
+  all_nodes = [Node() for i in range(0, num_nodes)]
 
   for i in range(0, len(nodes)):
 
@@ -32,7 +37,8 @@ with open(antimony_file, "r+") as json_file:
     inspector = child['inspector']
     script = child['script']
 
-    n = Node()
+    #n = Node()
+    n = all_nodes[i]
     n.uid = uid;
     n.name = name;
 
@@ -53,38 +59,22 @@ with open(antimony_file, "r+") as json_file:
 
       d = {"name": data_name, "val": data_expr}
 
-      n.parameters.append( d )
-      print "%d, %s" % (j, n.parameters[j])
+      n.props.append( d )
+      print "%d, %s" % (j, n.props[j])
       #print "    %s (%s) expr = %s uid = %s" % (data_name, data_type, data_expr, data_uid)
 
 
     print "inspector node position = (%.2f, %.2f)" % (inspector[0], inspector[1])
     print "num lines in script = %d" % len(script)
+    print "num of props: %d" % (len(n.props))
 
-
-
-
-    print "num of parameters: %d" % (len(n.parameters))
-
-    all_nodes.append( {"name": name, "obj": n } )
-    print "appended a new object with this many parameters: %d" % (len(all_nodes[i]['obj'].parameters))
-
-
-
-
-    n = all_nodes[i]['obj']
-    print "%d - param len = %d" % (i, len(n.parameters))
-
-    # why is the length of the parameter list keep incrementing?
-    # it keeps including items appended from the other nodes.
-    # WHY
 
 
 
   print "------"
 
-  print "first node: %s, uid = %s parameters #0 name: %s" % (all_nodes[0]['name'], all_nodes[0]['obj'].uid, all_nodes[0]['obj'].parameters[0]['name'] )
-  print "2nd node: %s, uid = %s parameters #0 name: %s" % (all_nodes[1]['name'], all_nodes[1]['obj'].uid, all_nodes[1]['obj'].parameters[0]['name'] )
+  print "first node: %s, uid = %s, props #0 name: %s" % (all_nodes[0].name, all_nodes[0].uid, all_nodes[0].props[0]['name'] )
+  print "2nd node: %s, uid = %s, props #0 name: %s" % (all_nodes[1].name, all_nodes[1].uid, all_nodes[1].props[0]['name'] )
 
 
 
