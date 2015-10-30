2669e5bb2dab42fd3da72d2e34a149886352ce69
RobotGrrl <erin@robotgrrl.com>
2015-10-30 18:58:06 -0400

    some more thoughts and parsing

--- expr_eval.py
+++ expr_eval.py
@@ -1,3 +1,5 @@
+import re
+
 
 c1_x = -3.4
 c1_y = -1.27
@@ -21,3 +23,27 @@ expr4 = new2
 res1 = eval(expr4)
 print "4: %s" % (res1)
 
+print "-------"
+print "5:"
+
+#expr5 = "c1.x + c1.y"
+expr5 = "c1.x+c1.y"
+searchfor = "c1"
+found_ind = 0
+all_finds = []
+for i in range(0, len(expr5)):
+  res = expr5.find(searchfor, found_ind, len(expr5))
+  if res == -1:
+    break
+  else:
+    print "found %s at %d" % (searchfor, res)
+    found_ind = res+1
+    all_finds.append(found_ind)
+
+split_str = expr5.split(searchfor)
+for i in range(0, len(split_str)):
+  # removing the .'s and +'s (basically anything not alpha numeric)
+  # from the strings
+  split_str[i] = re.sub('[^0-9a-zA-Z_-]+', '', split_str[i])
+
+print split_str
