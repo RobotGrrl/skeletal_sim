fe762b683e39997304616d21dda0763c39b3aaaa
RobotGrrl <erin@robotgrrl.com>
2015-10-30 17:33:48 -0400

    stuck on a problem

--- compute_params.py
+++ compute_params.py
@@ -24,8 +24,6 @@ with open(antimony_file, "r+") as json_file:
 
   for i in range(0, len(nodes)):
 
-    n = Node()
-
     child = nodes[i]
 
     uid = child['uid']
@@ -34,6 +32,7 @@ with open(antimony_file, "r+") as json_file:
     inspector = child['inspector']
     script = child['script']
 
+    n = Node()
     n.uid = uid;
     n.name = name;
 
@@ -46,36 +45,55 @@ with open(antimony_file, "r+") as json_file:
     for j in range(0, len(datums)):
 
       data = datums[j]
-      # add these ^ to the parameters
-
+      
       data_uid = data['uid']
       data_name = data['name']
       data_expr = data['expr']
       data_type = data['type']
 
-      n.parameters.append( {"name": data_name, "val": data_expr} )
+      d = {"name": data_name, "val": data_expr}
 
-      print "    %s (%s) expr = %s uid = %s" % (data_name, data_type, data_expr, data_uid)
+      n.parameters.append( d )
+      print "%d, %s" % (j, n.parameters[j])
+      #print "    %s (%s) expr = %s uid = %s" % (data_name, data_type, data_expr, data_uid)
+
 
     print "inspector node position = (%.2f, %.2f)" % (inspector[0], inspector[1])
     print "num lines in script = %d" % len(script)
 
-    all_nodes.append( {"name": name, "obj": n} )
+
+
+
+    print "num of parameters: %d" % (len(n.parameters))
+
+    all_nodes.append( {"name": name, "obj": n } )
+    print "appended a new object with this many parameters: %d" % (len(all_nodes[i]['obj'].parameters))
+
+
+
+
+    n = all_nodes[i]['obj']
+    print "%d - param len = %d" % (i, len(n.parameters))
+
+    # why is the length of the parameter list keep incrementing?
+    # it keeps including items appended from the other nodes.
+    # WHY
+
+
+
+  print "------"
 
   print "first node: %s, uid = %s parameters #0 name: %s" % (all_nodes[0]['name'], all_nodes[0]['obj'].uid, all_nodes[0]['obj'].parameters[0]['name'] )
+  print "2nd node: %s, uid = %s parameters #0 name: %s" % (all_nodes[1]['name'], all_nodes[1]['obj'].uid, all_nodes[1]['obj'].parameters[0]['name'] )
 
 
 
-  # x = MyClass()
-  # print x.i
 
-  # x.parameters.append( {"name": "x", "val": -3.4} )
-  # x.parameters.append( {"name": "y", "val": -1.27} )
-  # print len(x.parameters)
 
-  # all_nodes.append( {"name": "cool name", "obj": x} )
-  # print all_nodes[0]['name']
-  # print all_nodes[0]['obj'].i
+
+
+
+
 
 
 
